e6ac07f3eb3a14048222c3cf04ca51345a57dda5
Chris Fisher <chris.i.fisher@gmail.com>
2023-07-26 15:19:46 +1200

    Remove console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,7 @@ export default function Home() {
 
         setAccessToken(token);
       })
-      .catch((err) => {
-        console.log(err);
-
+      .catch(() => {
         router.push("/sign-in");
       });
   }, [router]);
